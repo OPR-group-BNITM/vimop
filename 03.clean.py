5f241ba0d940015772927b7b0cb5ac0139b172c3
Annick <annick.renevey@bnitm.de>
2020-08-28 14:16:57 +0200

    Consensus

--- 03.clean.py
+++ 03.clean.py
@@ -69,9 +69,9 @@ for sample in SAMPLES:
         # stats = pd.DataFrame(, data=(re.sub(',','',lines[1])).split()[3:8])
         stats = stats.append(pd.Series((re.sub(',','',lines[1])).split()[3:8], index=['num_seqs', 'sum_len', 'min_len', 'avg_len','max_len']), ignore_index=True
         if (i == 0):
-           stats['step'] = SPEC[i]
-        else:
-            stats['step'].iloc[-1] = SPEC[i]
+            stats['step'] = SPEC[i]
+        # else:
+            # stats['step'].iloc[-1] = SPEC[i]
         print(SPEC)
         print(i)
         i+=1
