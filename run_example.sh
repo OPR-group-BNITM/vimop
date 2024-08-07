5dfb0ce188c6f5bef74cd096a7e3702a4e83945c
Nils Petersen <nils.petersen@EDVID-1260.local>
2024-08-07 18:03:30 +0200

    wip

--- run_example.sh
+++ run_example.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-nextflow main.nf --fastq /Volumes/Samsung_T5/nanoflow_testsets/CKY-2023-RUN091/pass/barcode20
-
-# nextflow main.nf \
-#     --fastq /Users/nils.petersen/dev/metagenomics_pipeline/nextflow_testdata/tinytest/pass \
-#     --sample_sheet  /Users/nils.petersen/dev/metagenomics_pipeline/nextflow_testdata/tinytest/samplesheet.csv 
+nextflow main.nf --fastq /Users/nils.petersen/dev/metagenomics_pipeline/nextflow_testdata/tinytest/pass
