388b07b9b01b22d4afe37f41e42ec9439db66aa6
mlmial <ml.miale@icloud.com>
2025-03-07 15:35:17 +0100

    change image tag to dockerhub equivalent

--- images/build_images.sh
+++ images/build_images.sh
@@ -5,6 +5,6 @@ images=(canu centrifuge general ingress medaka report)
 for img in "${images[@]}";
 do
     echo $img
-    docker build --no-cache -t opr_${img}:0.0.1 $img/
+    docker build --no-cache -t oprgroup/${img}:0.0.1 $img/
     echo ""
 done
